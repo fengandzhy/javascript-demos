474fd5837873c76a2ba35e4fa0b6135d19dcd8ea
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-06-19 08:18:18 +0800

    complete the comment about this in arrow functions ChenXuHui chapter02 demo05

--- ECMAScript6.0/ChenXueHui/chapter02/demo05.js
+++ ECMAScript6.0/ChenXueHui/chapter02/demo05.js
@@ -46,8 +46,17 @@
 
 
 /**
- * 关于箭头函数的this
+ * 关于箭头函数的this,箭头函数没有this，在箭头函数里的this 绑定的就是最近一层非箭头函数的 this
  *
+ * 例如在show4里面的this, 它最近一层的非箭头函数是show3, 所以show4里面的this的指向，就相当于调用show3时，show3里面的this指向
+ * 所以，执行dog.show3(); 如果show3里面有this，此时指向的就是dog，所以show3里面的箭头函数show4的this也就指向dog
+ * 在打个比方，如果
+ * var test = dog.show3;
+ * test();
+ * 如果show3里面有this，此时指向的就是undefined(非严格模式下是window)，所以所以show3里面的箭头函数show4的this也就指向window
+ *
+ * 例如在show2里面的this,show2外层没有箭头函数，它里面的this就是父级作用域里的this, show2的父级作用域是谁？好像是个块级作用域？还是全局作用域?
+ * anyway，不管是块级作用域还是全局作用域里面的this指向的都是window
  *
  * */
 {
@@ -70,8 +79,15 @@
     dog.show1();
     dog.show2();
     dog.show3();
+    var test = dog.show3;
+    test();
 }
 
+/**
+ * 同理这里的this指向的是window，是箭头函数a父级作用域里面的this指向，a的父级作用域是块级作用域，里面的this指向window
+ * 在浏览器console中运行下面代码,显示输出window
+ * {console.log(this)}
+ * */
 {
     const obj = {
         a: () => {
@@ -81,6 +97,10 @@
     obj.a()
 }
 
+/**
+ * 与上同理，箭头函数fn里的this是其父级作用域里的this,其父级作用域是getAge函数，所以运行obj.getAge()时fn里面的this指向就跟
+ * 运行obj.getAge()时getAge里面的this指向一致，所以是1990
+ * */
 {
     var obj = {
         birth: 1990,
