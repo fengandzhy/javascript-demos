69348bd15d0d82ac45ab176e2e5c3e25b3cf208f
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-06-21 03:04:34 +0800

    add setInterval and setTimeout to liyou classic chapter07

---
+++ JavaScriptES5/liyou/classic/chapter07/setInterval.js
@@ -0,0 +1,13 @@
+
+/**
+ * 第一个参数是函数
+ * 第二个参数是执行间隔
+ * 之后的参数全部都是函数的参数
+ * */
+var a =10;
+var b =15;
+
+setInterval(function(b,a){
+    console.log(a);
+},1000,a,b)
+
---
+++ JavaScriptES5/liyou/classic/chapter07/setTimeout.js
@@ -0,0 +1,12 @@
+
+/**
+ * 只执行一遍的定时器，定时器永远只放在最后执行
+ * */
+setTimeout(function () {
+    console.log("abc");
+},100);
+
+// setInterval(function () {
+//     console.log('abcc');
+// },1000);
+
