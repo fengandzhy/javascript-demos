de62f6a5b65240de5a735b94f8115ddb50afa3b2
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2020-02-17 08:04:56 +1300

    javascript datatype

---
+++ JavaScriptES5/liyou/classic/JavaScript1/datatype.js
@@ -0,0 +1,27 @@
+
+
+/**
+ * 
+ * JS里面六种数据类型
+ * @type {String}
+ */
+var datatype = "str";
+console.log(typeof(datatype));
+
+var datatype = 3;
+console.log(typeof(datatype));
+
+var datatype = true;
+console.log(typeof(datatype));
+
+var datatype = function(){
+};
+console.log(typeof(datatype));
+
+var datatype = {}
+console.log(typeof(datatype));
+
+var datatype = undefined
+console.log(typeof(datatype));
+
+
