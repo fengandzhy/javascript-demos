93f5214337797e7003bbd25b6f83c008186dc47b
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2020-07-13 05:35:30 +1200

    this test

---
+++ ECMAScript6.0/Test/this/demo01.js
@@ -0,0 +1,19 @@
+
+
+class Cat {
+    sayThis () {
+        console.log(this); // 这里的 `this` 指向谁？
+    }
+
+    exec (cb) {
+        cb();
+    }
+
+    render () {
+        this.exec(this.sayThis);
+    }
+}
+
+const tom = new Cat();
+tom.render();
+
