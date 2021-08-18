545f04edc606851273d24ad0a822a244c7c6f8f6
fengandzhy <fengandzhy@gmail.com>
2021-08-18 20:17:36 +1200

    string demo02

---
+++ javascript/string/string-demo02.js
@@ -0,0 +1,10 @@
+/**
+ * 1.charCodeAt是返回指定索引处的字符的Unicode, 索引是从0开始, 如果超出字符串长度则返回NaN
+ *
+ * */
+let str = "Hello world!";
+let res = str.charCodeAt(1)
+console.log(res);
+res = str.charCodeAt(100);
+console.log(res);
+
