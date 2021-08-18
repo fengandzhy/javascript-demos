a71ccb6cc6e9d90f8ed549687f7682a9bd19dd37
fengandzhy <fengandzhy@gmail.com>
2021-08-18 20:14:49 +1200

    string demo01

---
+++ javascript/string/string-demo01.js
@@ -0,0 +1,12 @@
+/**
+ * 1.charAt是返回指定索引处的字符串, 索引是从0开始, 如果超出字符串长度则返回空字符串
+ * 
+ * */
+let str = "Hello world!";
+let res = str.charAt(1);
+console.log(res);
+res = str.charAt(100);
+console.log(res==='');
+
+
+
