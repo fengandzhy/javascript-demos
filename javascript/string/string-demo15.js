d97e50eb6c6a0da35c5e08ed776f0c5d6fbc15ec
fengandzhy <fengandzhy@gmail.com>
2021-08-20 21:09:58 +1200

    javascript-string

--- javascript/string/string-demo15.js
+++ javascript/string/string-demo15.js
@@ -1,9 +1,8 @@
 /**
- * 1. search 似乎跟indexOf方法一样, 返回的是匹配的位置.
+ * 1. slice 返回一个新字符串, 这个字符串是原字符串提取的部分, 例如原字符串str.slice(0,6);
+ * 返回的就是原字符串索引从0开始到索引6, 其中索引6是不包含的.
  *
  * */
 let str = 'visit Medium!';
-let res = str.search('Medium');
-console.log(res);
-res = str.search('aaa');
+let res = str.slice(0,6);
 console.log(res);
