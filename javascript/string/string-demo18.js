29b85ba99d0303400bdbfb8dc2779712a7184a4b
fengandzhy <fengandzhy@gmail.com>
2021-08-21 21:19:07 +1200

    javascript-string

---
+++ javascript/string/string-demo18.js
@@ -0,0 +1,10 @@
+/**
+ * 1. substr 例如str.substring(0,6);就表示返回从索引0开始到索引6为止的字符串,其中不包含索引为6的字符
+ * 那么str.substring(1,6); 就表示返回从索引1开始到索引6为止的字符串
+ *
+ * */
+let str = 'visit Medium!';
+let res = str.substring(0,6);
+console.log(res);
+res = str.substring(1,6);
+console.log(res);
---
+++ javascript/string/string-demo20.js
@@ -0,0 +1,7 @@
+/**
+ * 1. toLocaleUpperCase 将字符串转换成大写字符
+ *
+ * */
+let str = 'Hello World!';
+let res = str.toLocaleUpperCase()
+console.log(res);
