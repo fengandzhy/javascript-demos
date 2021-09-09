878759c890ff8383008a2553b5751cb45f47fbcf
fengandzhy <fengandzhy@gmail.com>
2021-09-10 10:30:11 +1200

    javascript-array

--- javascript/array/array-demo05.js
+++ javascript/array/array-demo05.js
@@ -1,11 +1,17 @@
 /**
+ * 1. arr.slice 剪切数组，返回剪切之后的数组，元素不会改变
+ *  1) 传入一个参数，表示起始位置，结束位置为最末尾
+ *  2) 传入2个参数，表示起始位置与结束位置，但不包括结束位置所在的元素
  *
  * */
 
 var colors = ['red','blue','green'];
-colors.slice(2); //['green']
+var arr=colors.slice(2);
+console.log(colors);
+console.log(arr);
 
 
-
-
-
+var colors = ['red','blue','green'];
+var arr=colors.slice(1,2);
+console.log(colors);
+console.log(arr);
