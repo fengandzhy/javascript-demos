31c79a109424a77806ba8d03d25580582a0be5ba
fengandzhy <fengandzhy@gmail.com>
2021-09-28 20:54:03 +1300

    javascript-array

--- javascript/array/array-demo01.js
+++ javascript/array/array-demo01.js
@@ -1,14 +1,27 @@
 
 /**
  * 1. 定义数组的方法
+ *
+ * 2. 在js中相等的只能值字面量, 两个object的东西永远不会相等. 返回会是false
+ * 例如 d和e 这连个就是object var d = new String('abc');  var e = new String('abc')
+ * 所以 console.log(d==e);
+ * 另外 c 就是字面量 var c = 'abc' 当执行console.log(typeof c );返回为string
+ * 当一方有字面量的情况下 这种表达式的返回console.log(c==d); 才有可能是true
+ *
+ * 对于数组来说 var a = [5,1,'abc']; var b = new Array(5,1,'abc'); 他们都是object 所以 console.log(a==b); 返回为false
+ * 这个是js 的内部机制造成的
+ *
+ *
  * */
 var a = [5,1,'abc'];
 var b = new Array(5,1,'abc');
-console.log(a==b);
+console.log(a==b); // false
 
 var c = 'abc';
 var d = new String('abc');
-console.log(c==d);
+var e = new String('abc')
+console.log(c==d); //true
+console.log(d==e); //false
 
 var obj = new String('0');
 var str = '0';
