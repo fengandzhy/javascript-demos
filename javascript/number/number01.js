c8f717325e9702879b31ed9e9eb7a5dc62b09810
fengandzhy <fengandzhy@gmail.com>
2021-09-28 21:10:45 +1300

    javascript-number

---
+++ javascript/number/number01.js
@@ -0,0 +1,25 @@
+
+
+console.log(true == 1);//true
+console.log(true == 2); //false
+console.log(false == 0); //true
+
+if(2){
+    console.log('ABC'); //打印
+}
+
+if(0){
+   console.log('DEF');
+}
+
+if('a'){
+    console.log('123');
+}
+
+if(undefined){
+    console.log('456');
+}
+
+if(null){
+    console.log('789');
+}
