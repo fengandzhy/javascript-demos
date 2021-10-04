0dfe71a60eb4f9cdedccc1c48f5171b1ca5a08b5
fengandzhy <fengandzhy@gmail.com>
2021-10-05 08:30:07 +1300

    object-demo03

--- javascript/object/object-demo03.js
+++ javascript/object/object-demo03.js
@@ -1,3 +1,14 @@
+
+/**
+ * 在javascript中关于这个object的赋值就是这种形式的
+ * let 变量名 = {
+ *     key1:value1,
+ *     key2:value2
+ * }
+ * 当然这个key1 key2都是变量名,按照变量名规则可以随便写, 这里的value1和value2就是字面量或者对象了
+ * 在stu里是字面量, 在model里就是对象
+ *
+ * */
 let stu = {
     name:'张三',
     age: 18
