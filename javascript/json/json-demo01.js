fa8be9832c4628b181bff6dfef6b1ca9f71f9898
fengandzhy <fengandzhy@gmail.com>
2021-10-08 11:53:15 +1300

    json

--- javascript/json/json-demo01.js
+++ javascript/json/json-demo01.js
@@ -14,6 +14,7 @@ console.log(JsonFromFunction.name);
 
 /**
  * 使用JSON.parse将字符串转化成json
+ * 这个是严格遵守json规范的, '{ "name": "小强", "age": 16 }' 就跟这个一样,外面用单引号, 里面的key值用双引号.
  * */
 const JsonFromJSON = JSON.parse(strJson);
 console.log(JsonFromFunction.name);
