0f7a5494ac4c468893ea90fffa9ddcc4f3b1b684
fengandzhy <fengandzhy@gmail.com>
2021-12-04 14:56:16 +1300

    object-demo01

--- javascript/object/object-demo01.js
+++ javascript/object/object-demo01.js
@@ -18,6 +18,10 @@ f1.fun();
 
 /**
  * 2. ch是checkscope new出来的, 但是在checkscope里面没有东西绑定到this上, 所以ch输出就是checkscope {} 里面没有任何属性
+ *
+ * 3.如果想运行 checkscope.f(); 那么这个函数f必须定义在checkscope的原型对象上.
+ *
+ *
  * */
 function checkscope(){
     var scope = 'local scope';
@@ -32,3 +36,10 @@ var ch = new checkscope();
 console.log(ch);
 var a = checkscope();
 console.log(a);
+
+
+checkscope.__proto__.f = function(){
+    console.log('We are here');
+}
+
+checkscope.f();
