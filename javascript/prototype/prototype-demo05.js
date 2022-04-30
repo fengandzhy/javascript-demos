23de98696882956ef1913cec4693ee0bdb0747a0
fengandzhy <fengandzhy@gmail.com>
2022-04-30 19:43:41 +1200

    javascript prototype

--- javascript/prototype/prototype-demo05.js
+++ javascript/prototype/prototype-demo05.js
@@ -1,9 +1,13 @@
 function Person(){}
 console.log(Person);
 console.log(Person.__proto__);
-console.log(Function);
-console.log(Function.prototype);
-console.log(Function.__proto__);
-console.log(Object); //ƒ Object()
-console.log(Object.__proto__); //ƒ Object()
-console.log(Object.prototype); //ƒ Object()
+console.log(Person.prototype);
+console.log(typeof Person); //function
+// var fn = new Function();
+// console.log(fn);
+// console.log(Function);
+// console.log(Function.prototype);
+// console.log(Function.__proto__);
+// console.log(Object); //ƒ Object()
+// console.log(Object.__proto__); //ƒ Object()
+// console.log(Object.prototype); //ƒ Object()
