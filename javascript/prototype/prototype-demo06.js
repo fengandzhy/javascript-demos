a194307bac120b798b566fce529742cdd4770708
fengandzhy <fengandzhy@gmail.com>
2022-04-30 20:08:43 +1200

    javascript prototype

---
+++ javascript/prototype/prototype-demo06.js
@@ -0,0 +1,7 @@
+// var fn = new Function('return x;');
+// console.log(fn);
+console.log(Function);
+console.log(Function.prototype);
+console.log(Function.__proto__);
+console.log(Function.prototype === Function.__proto__); //true
+console.log(typeof Function);
