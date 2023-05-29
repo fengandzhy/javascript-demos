acf22aa95beebc9b56fff10fa3637607e2dc0348
fengandzhy <fengandzhy@gmail.com>
2023-05-29 21:15:04 +1200

    js-closure

--- js-demos/js-prototype/prototype-demo07.js
+++ js-demos/js-prototype/prototype-demo07.js
@@ -11,21 +11,43 @@
  *
  * */
 function Person(){}
-console.log(Object); //ƒ Object()
-console.log(Object.__proto__); //ƒ Object()
-console.log(Object.prototype); //ƒ Object()
-console.log(typeof Object);//function
-console.log(Object.__proto__ === Function.prototype); //true
-console.log(Object instanceof Function);//true
-console.log(Person instanceof Function);//true
-console.log(Function instanceof Object);//true
-console.log(Function.__proto__ === Object.__proto__); //true
-console.log(Object instanceof Object);//true
-console.log(Function instanceof Object);//true
-console.log(Person.__proto__ === Function.prototype);//true
-console.log(Function); //ƒ Function()
-console.log(typeof Function);//function
-console.log(Person);//ƒ Person()
-console.log(typeof Person);//function
-console.log(Function.__proto__ === Person.__proto__);//true
-console.log(Person.constructor);
+var person = new Person();
+console.log(person); // {}
+console.log(person.__proto__); // {}
+console.log(person.__proto__.__proto__);
+console.log(person.constructor);
+
+console.log(Person);
+console.log(Person instanceof Function);
+console.log(Person instanceof Object);
+console.log(Person.__proto__ === Function.prototype);
+console.log(Person.__proto__.__proto__ === Object.prototype);
+console.log(Person.constructor === Function);
+console.log(Person.__proto__.constructor === Object);
+console.log(Function.constructor === Function);
+console.log(Function.__proto__.__proto__.constructor === Object);
+// console.log(Object); //ƒ Object()
+// console.log(Object.__proto__); //ƒ Object()
+// console.log(Object.prototype); //ƒ Object()
+// console.log(typeof Object);//function
+// console.log(Object.__proto__ === Function.prototype); //true
+// console.log(Object instanceof Function);//true
+// console.log(Person instanceof Function);//true
+// console.log(Function instanceof Object);//true
+// console.log(Function.__proto__ === Object.__proto__); //true
+// console.log(Object instanceof Object);//true
+// console.log(Function instanceof Object);//true
+// console.log(Person.__proto__ === Function.prototype);//true
+// console.log(Function); //ƒ Function()
+// console.log(typeof Function);//function
+// console.log(Person);//ƒ Person()
+// console.log(typeof Person);//function
+// console.log(Function.__proto__ === Person.__proto__);//true
+// console.log(Person.constructor);
+// console.log(Person.constructor === Function.constructor); // true
+// console.log(Function === Function.constructor); //true
+// console.log(Object.constructor === Function.constructor); //true
+// console.log(person.constructor === Person); //true
+// console.log(Function.__proto__ ===  Object.prototype); //true
+
+
