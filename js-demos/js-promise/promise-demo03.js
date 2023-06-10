831ab4d369a10d134b549d3faa83ac45ddd14640
fengandzhy <fengandzhy@gmail.com>
2023-06-10 20:39:24 +1200

    js-promise catch

---
+++ js-demos/js-promise/promise-demo03.js
@@ -0,0 +1,13 @@
+const p = new Promise((resolve, reject) => {
+    // reject('failed') // 执行reject 方法会让promise 的状态变为 rejected.
+    // console.log(a); // 执行出错 也会让promise 的状态变为 rejected.
+    throw new Error('error'); // 人为抛出Error,  也会让promise 的状态变为 rejected.
+});
+
+/**
+ * catch 方法， 这个方法的参数也是一个函数，当这个函数只有在promise 对象为 rejected 的时候才被执行，
+ *
+ * */
+p.catch((err) => {
+    console.log('In the catch method.', err);
+});
