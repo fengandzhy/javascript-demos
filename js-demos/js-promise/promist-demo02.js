44174a9de2ea1c63a119617c7cef0562baad9d36
fengandzhy <fengandzhy@gmail.com>
2023-06-10 20:30:59 +1200

    js-promise then

---
+++ js-demos/js-promise/promist-demo02.js
@@ -0,0 +1,19 @@
+const p = new Promise((resolve, reject) => {
+    resolve('success');
+});
+
+const t = p.then((value) => {
+    // 成功执行这个方法会把t 的状态变成 fulfilled, 但是如果这个方法没有被成功执行,它会把t 的状态变成 rejected, 然后会执行t 的then里面第二个方法
+    console.log('p success is', value);
+    return value; // 这里的return 值就是 t 的then 里面第一个函数的传入参数.
+}, (err) => {
+    // 当promise对象的桩体是rejected 的时候执行, 形参err是reject 传进来的.
+    console.log('p error is', err);
+});
+
+t.then((value) => {
+    console.log('t success is', value);
+}, (err) => {
+
+    console.log('t error is', err);
+});
