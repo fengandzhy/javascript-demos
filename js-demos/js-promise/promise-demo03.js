f988a669698a8be78533344bceaedf8df96188e3
fengandzhy <fengandzhy@gmail.com>
2023-06-22 20:02:35 +1200

    js-async-await a demo promise then

--- js-demos/js-promise/promise-demo03.js
+++ js-demos/js-promise/promise-demo03.js
@@ -3,7 +3,7 @@ const p = new Promise((resolve, reject) => {
     // console.log(a); // 执行出错 也会让promise 的状态变为 rejected.
     throw new Error('error'); // 人为抛出Error,  也会让promise 的状态变为 rejected.
 });
-
+console.log(p);
 /**
  * catch 方法， 这个方法的参数也是一个函数，当这个函数只有在promise 对象为 rejected 的时候才被执行，
  *
