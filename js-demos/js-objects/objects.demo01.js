680d81a985d922fb64fc44b4c04b24282608726f
fengandzhy <fengandzhy@gmail.com>
2023-07-13 10:57:06 +1200

    js-object objects.assign

--- js-demos/js-objects/objects.demo01.js
+++ js-demos/js-objects/objects.demo01.js
@@ -5,4 +5,14 @@ const target = { a: 1 };
 Object.assign(target, {b: 2});
 console.log(target);
 
+/**
+ * 注意，如果目标对象与源对象有同名属性，或多个源对象有同名属性，则后面的属性会覆盖前面的属性。
+ * */
+const target = { a: 1 };
+const source1 = { b: 2 };
+const source2 = { c: 3 };
+
+Object.assign(target, source1, source2);// {a:1, b:2, c:3}
+
+
 
