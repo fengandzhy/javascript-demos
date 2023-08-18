2c6b3b7319355c66a4aa2c0b20fb29397dde7ba9
fengandzhy <fengandzhy@gmail.com>
2023-08-18 16:12:38 +1200

    js-this js-array-03.js

--- js-demos/js-array/js-array-03.js
+++ js-demos/js-array/js-array-03.js
@@ -12,3 +12,21 @@ console.log(item);   // Sean
 console.log(arr);   // ["Lily", "lucy", "Tom", "Jack"]
 
 
+/**
+ * shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。
+ * unshift() 方法可向数组的开头添加一个或更多元素，并返回新的长度。
+ * */
+var arr = ["Lily","lucy","Tom"];
+var count = arr.unshift("Jack","Sean");
+console.log(count);   // 5
+console.log(arr);   //["Jack", "Sean", "Lily", "lucy", "Tom"]
+var item = arr.shift();
+console.log(item);   // Jack
+console.log(arr);   // ["Sean", "Lily", "lucy", "Tom"]
+
+
+
+
+
+
+
