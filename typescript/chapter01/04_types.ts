bddb650aa86f0cc950ed16febec2e7f9dd41d7c7
fengandzhy <fengandzhy@gmail.com>
2023-11-28 07:36:18 +1300

    04_types.ts

--- typescript/chapter01/04_types.ts
+++ typescript/chapter01/04_types.ts
@@ -20,7 +20,39 @@ console.log(b3);
 let b4: {name: string, [propName: string]: any}; // 它除了有个name 是string 之外, 其余的, 随便
 b4 = {name: '猪八戒', age: 18, gender: '男'};
 
+/**
+ * 定义b5 为一个函数类型, 该函数有两个number的参数, 然后有一个number类型的返回值,
+ * 具体语法：(形参:类型, 形参:类型 ...) => 返回值
+ * */
+let b5: (a:number,b:number) => number;
 
+// 给该函数具体赋值
+b5 = (c:number,d:number) : number =>{
+    return c + d;
+};
+
+/*
+*   数组的类型声明：
+*       类型[]
+*       Array<类型>
+* */
+let b6:number[];
+b6 = [1,2,3];
+console.log(b6);
+
+let b7:Array<object>;
+b7 = [{a:1},function b(){},[1,2,3]];
+b7.push({a:8});
+console.log(b7);
+
+/*
+*   元组，元组就是固定长度的数组
+*       语法：[类型, 类型, 类型]
+* */
+let b8: [string, number];
+b8 = ['hello', 123];
+b8.push('b');
+console.log(b8); // 跟数组一样它是可以push 的
 
 
 
