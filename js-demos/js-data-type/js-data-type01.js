49ff7478030a77bad5f1f3a0893fe1e0b20c2e95
fengandzhy <fengandzhy@gmail.com>
2023-12-07 08:30:52 +1300

    js-this js-array-15.js

--- js-demos/js-data-type/js-data-type01.js
+++ js-demos/js-data-type/js-data-type01.js
@@ -7,9 +7,12 @@ console.log((2).__proto__ === Number.prototype); // true
 
 const v1 = Object.prototype.toString.call((2));
 const v2 = Object.prototype.toString.call(new Number(2));
+const v3 = Object.prototype.toString.call(2);
+
 
 console.log(v1);
 console.log(v2);
+console.log(v3);
 console.log(v1 == v2);
 console.log(v1 === v2);
-
+console.log(v3 === v2);
