754ab24cf039afd0a51d91e60d7a37909e491d24
fengandzhy <fengandzhy@gmail.com>
2023-12-07 07:32:08 +1300

    js-this js-array-15.js

--- js-demos/js-prototype/prototype-demo09.js
+++ js-demos/js-prototype/prototype-demo09.js
@@ -5,7 +5,10 @@
 function Person(){}
 console.log(Function); // Function对象本身
 console.log(Function.__proto__); // 所有函数的原形对象, 它的constructor属性是 Function对象本身
+console.log(Function.__proto__ === Function.prototype); // true
+console.log(Function instanceof Function); // true
 console.log(Function.__proto__.__proto__); // 所有对象的原形对象
 console.log(Function.constructor === Function); // true
 console.log(typeof Function.__proto__ === 'function'); // true
+console.log(typeof Function.__proto__.__proto__ === Function.prototype); //false
 console.log(typeof Function.__proto__ instanceof Function); // false
