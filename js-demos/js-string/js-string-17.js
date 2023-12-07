29b12ab2d39eb9076048ea80d2667ae67aa81031
fengandzhy <fengandzhy@gmail.com>
2023-12-08 07:59:36 +1300

    js-this js-array-15.js

---
+++ js-demos/js-string/js-string-17.js
@@ -0,0 +1,10 @@
+/**
+ * 1. substr 例如str.substr(0,6);就表示返回从索引0开始往后6位长度的字符
+ * 那么str.substr(1,6); 就表示返回从索引1开始往后6位长度的字符
+ *
+ * */
+let str = 'visit Medium!';
+let res = str.substr(0,6);
+console.log(res);
+res = str.substr(1,6);
+console.log(res);
