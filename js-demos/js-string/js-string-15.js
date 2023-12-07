4fefa2a6ab661f23c06b8c719cb571594dc9e385
fengandzhy <fengandzhy@gmail.com>
2023-12-08 07:58:28 +1300

    js-this js-array-15.js

---
+++ js-demos/js-string/js-string-15.js
@@ -0,0 +1,8 @@
+/**
+ * 1. slice 返回一个新字符串, 这个字符串是原字符串提取的部分, 例如原字符串str.slice(0,6);
+ * 返回的就是原字符串索引从0开始到索引6, 其中索引6是不包含的.
+ *
+ * */
+let str = 'visit Medium!';
+let res = str.slice(0,6);
+console.log(res);
