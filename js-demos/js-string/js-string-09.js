d841c5c7da8c3ab5e589079a995c873e80fcd417
fengandzhy <fengandzhy@gmail.com>
2023-12-08 07:53:21 +1300

    js-this js-array-15.js

---
+++ js-demos/js-string/js-string-09.js
@@ -0,0 +1,8 @@
+/**
+ * 1. localeCompare 比较两个字符串的排序, 如果str1在str2之前则返回-1, 如果两个字符串相等则返回0, 如果str1 在str2之后则返回 1
+ *
+ * */
+let str1 = 'ab';
+let str2 = 'cd';
+let res = str1.localeCompare(str2);
+console.log(res);
