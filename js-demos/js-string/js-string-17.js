ea523626f9fa7566366118c7fb3809cf9435c878
fengandzhy <fengandzhy@gmail.com>
2023-12-08 08:02:48 +1300

    js-this js-array-15.js

--- js-demos/js-string/js-string-17.js
+++ js-demos/js-string/js-string-17.js
@@ -1,10 +1,12 @@
 /**
  * 1. substr 例如str.substr(0,6);就表示返回从索引0开始往后6位长度的字符
  * 那么str.substr(1,6); 就表示返回从索引1开始往后6位长度的字符
- *
+ * str.substr(1); 表示从索引1开始一直到末尾的这段字符串
  * */
 let str = 'visit Medium!';
 let res = str.substr(0,6);
 console.log(res);
 res = str.substr(1,6);
+console.log(res);
+res = str.substr(1);
 console.log(res);
--- js-demos/js-string/js-string-18.js
+++ js-demos/js-string/js-string-18.js
@@ -1,7 +1,7 @@
 /**
  * 1. substr 例如str.substring(0,6);就表示返回从索引0开始到索引6为止的字符串,其中不包含索引为6的字符
  * 那么str.substring(1,6); 就表示返回从索引1开始到索引6为止的字符串
- *
+ * str.substring(1); 表示从索引1开始一直到末尾的这段字符串
  * */
 let str = 'visit Medium!';
 let res = str.substring(0,6);
