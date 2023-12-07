51744cc494cb3e4c9aef5bbca5f4b9cb166fc63f
fengandzhy <fengandzhy@gmail.com>
2023-12-08 07:52:18 +1300

    js-this js-array-15.js

---
+++ js-demos/js-string/js-string-08.js
@@ -0,0 +1,8 @@
+/**
+ * 1. lastIndexOf 返回指定值在字符串中最后一次出现的索引位置, 这个索引是从0开始计算,
+ * 如果要搜索的值从未出现, 则此方法返回-1
+ *
+ * */
+let str = "Hello world, welcome to the university.";
+let res = str.lastIndexOf('e');
+console.log(res);
